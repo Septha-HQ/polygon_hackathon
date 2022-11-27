33cbaa5371013919ac53ec286b5afd310354353a
igori <igorisolomon@gmail.com>
2022-11-27 14:57:45 +0100

    Added local smartcontract ABI

---
+++ web/src/utils/constants.js
@@ -0,0 +1,12 @@
+// Contract deployment: Transaction
+//   Contract address:    0x5fbdb2315678afecb367f032d93f642f64180aa3
+//   Transaction:         0x7d5cb4c6488e4a51b0bc9bad927fb4a147eeb3ab37bbd855dcab546b9dd457a5
+//   From:                0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266
+//   Value:               0 ETH
+//   Gas used:            1522563 of 1522563
+//   Block #1:            0x6d8737637b4f3eb23bd7849988e1ef76fe44a20382cef69428228f1792617e1c
+
+import abi from "./Transaction.json";
+
+export const contractABI = abi.abi;
+export const contractAddress = "0x5fbdb2315678afecb367f032d93f642f64180aa3";
