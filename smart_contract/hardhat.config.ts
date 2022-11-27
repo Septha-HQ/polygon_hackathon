39dc50d70702f48a2de3f13a650ed434a187c05c
igori <igorisolomon@gmail.com>
2022-11-27 13:04:02 +0100

    Local server set up

--- smart_contract/hardhat.config.ts
+++ smart_contract/hardhat.config.ts
@@ -3,20 +3,12 @@ import "./tasks/accounts"
 import * as dotenv from 'dotenv'
 dotenv.config()
 
-const MUMBAI_RPC_URL = process.env.MUMBAI_RPC_URL;
-const PRIVATEKEY1 = process.env.PRIVATEKEY1!;
-
 const config: HardhatUserConfig = {
-  defaultNetwork: "hardhat",
+  defaultNetwork: "localhost",
   networks: {
     localhost: {
       url: "http://127.0.0.1:8545/",
       chainId: 31337,
-    },
-    mumbai: {
-      url: MUMBAI_RPC_URL,
-      accounts: [PRIVATEKEY1],
-      chainId: 80001,
     }
   },
   solidity: "0.8.17",
