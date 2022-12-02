1a23dc53e35da7cbc9e340a38b3a966576375312
igori <igorisolomon@gmail.com>
2022-12-02 13:29:31 +0100

    Implement test coverage with Ganache-cli

--- smart_contract/hardhat.config.ts
+++ smart_contract/hardhat.config.ts
@@ -1,6 +1,7 @@
 import { HardhatUserConfig } from "hardhat/config";
-import "./tasks/accounts"
-import * as dotenv from 'dotenv'
+import "solidity-coverage";
+import "./tasks/accounts";
+import * as dotenv from 'dotenv';
 dotenv.config()
 
 const config: HardhatUserConfig = {
@@ -9,6 +10,10 @@ const config: HardhatUserConfig = {
     localhost: {
       url: "http://127.0.0.1:8545/",
       chainId: 31337,
+    },
+    localhost_test: {
+      url: "http://127.0.0.1:8545/",
+      chainId: 1337,
     }
   },
   solidity: "0.8.17",
