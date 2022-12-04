049bee958a802d213232b366237c9dee3dc3ea3c
igori <igorisolomon@gmail.com>
2022-12-04 22:46:51 +0100

    .

--- smart_contract/hardhat.config.ts
+++ smart_contract/hardhat.config.ts
@@ -4,16 +4,16 @@ import "./tasks/accounts";
 import * as dotenv from 'dotenv';
 dotenv.config()
 
-const MUMBAI_RPC_URL = process.env.MUMBAI_RPC_URL;
+const MUMBAI_RPC_URL = process.env.MUMBAI_RPC_URL!;
 const MUMBAI_CHAINID = 80001;
-const PRIVATEKEY1 = process.env.PRIVATEKEY1!;
+const PRIVATEKEY = process.env.PRIVATEKEY!;
 
 const config: HardhatUserConfig = {
   defaultNetwork: "mumbai",
   networks: {
     mumbai: {
       url: MUMBAI_RPC_URL,
-      accounts: [PRIVATEKEY1],
+      accounts: [PRIVATEKEY],
       chainId: MUMBAI_CHAINID,
     }
   },
